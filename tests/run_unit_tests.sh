14087331061521fb276824a596e1386083aa5143
Brett Kuskie <fullaxx@gmail.com>
2023-04-28 02:45:57 -0600

    Switch to clang++ for compiling tests_against_stl.cpp

--- tests/run_unit_tests.sh
+++ tests/run_unit_tests.sh
@@ -5,5 +5,5 @@ set -e
 clang -Wall unit_tests.c -o unit_tests
 ./unit_tests
 
-g++ -Wall tests_against_stl.cpp -o tests_against_stl
+clang++ -Wall tests_against_stl.cpp -o tests_against_stl
 ./tests_against_stl
